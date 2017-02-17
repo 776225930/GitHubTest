b1edff5c89fa96bb4062f0913d1500905b4ea870
Jiang <776225930@qq.com>
2017-02-17 22:55:47 +0800

    Price change to 5.0

--- app/src/main/java/com/example/customerview2/MainActivity.java
+++ app/src/main/java/com/example/customerview2/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		int price=3.0;
+		int price=5.0;
     }
 }
